044d8f3c4413154c43f940d3bbdbb5fd322fc010
Benjamin <benjamin@thelicensingproject.com>
2017-08-09 17:57:15 -0400

    Add project files.

---
+++ ClosedXMLWrapper/Excel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClosedXMLWrapper
+{
+    public class Excel
+    {
+        private ClosedXML.Excel.XLWorkbook workbook = null;
+
+        private ClosedXML.Excel.IXLWorksheet workSheet = null;
+
+        private ClosedXML.Excel.IXLRange range = null;
+
+        private ClosedXML.Excel.IXLTable table = null;
+
+        public Excel(FileInfo fullExcelPath)
+        {
+            this.workbook = new ClosedXML.Excel.XLWorkbook(fullExcelPath.DirectoryName);
+            this.workSheet = workbook.Worksheet(1);
+            this.range = workSheet.Range(workSheet.FirstCellUsed(), workSheet.LastCellUsed());
+            this.table = range.AsTable();
+        }
+
+        public DataTable AddColumnToDataTable()
+        {
+            return null;
+        }
+
+        public DataTable AddToDataTable()
+        {
+            return null;
+        }
+
+        public Dictionary<string, string> SearchForRow()
+        {
+            return null;
+        }
+
+        public Dictionary<string, string> SearchForValue()
+        {
+            return null;
+        }
+
+        public void InsertRow()
+        {
+            
+        }
+
+        public void DeleteRow()
+        {
+
+        }
+
+        public void DeleteValue()
+        {
+
+        }
+
+        public static void Create()
+        {
+
+        }
+    }
+}
